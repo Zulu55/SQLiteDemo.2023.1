6a0d45bc08cb481bb31d5d2b450fa1c15ee51cef
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-16 17:46:21 -0500

    Set up SQLite connection

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace SQLiteDemo
+{
+    public static class Constants
+    {
+        private const string dbFileName = "SQLite.db3";
+
+        public const SQLiteOpenFlags flags = SQLiteOpenFlags.ReadWrite |
+                                              SQLiteOpenFlags.Create |
+                                              SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, dbFileName);
+
+    }
+}
---
+++ Repository/CustomerRepository.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using SQLiteDemo.MVVM.Models;
+using static SQLite.SQLite3;
+
+namespace SQLiteDemo.Repository
+{
+    public class CustomerRepository
+    {
+        private readonly SQLiteConnection _connection;
+
+        public CustomerRepository()
+        {
+            _connection = new SQLiteConnection(Constants.DatabasePath, Constants.Flags);
+            _connection.CreateTable<Customer>();
+        }
+
+        public string StatusMessage { get; set; }
+
+        public void Add<T>(T model)
+        {
+            try
+            {
+                int result = _connection.Insert(model);
+                StatusMessage = $"{result} row(s) added.";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error {ex.Message}.";
+            }
+        }
+
+        public List<Customer> GetAll()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
